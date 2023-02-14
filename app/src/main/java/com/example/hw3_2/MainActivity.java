a8b41f39ae1ae168c3e360069315540a005adb03
Altynai78 <nurbekovaaltynai78@gmail.com>
2023-02-14 05:35:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hw3_2/MainActivity.java
@@ -0,0 +1,27 @@
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().add(R.id.conteiner,new First_Fragment()).commit();
+    }
+
+}package com.example.hw3_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
